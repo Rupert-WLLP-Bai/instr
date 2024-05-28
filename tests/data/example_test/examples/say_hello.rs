5f14732b6591fe28556b5afc8127b8c4904737a7
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2024-05-28 20:23:24 +0800

    initial commit

---
+++ tests/data/example_test/examples/say_hello.rs
@@ -0,0 +1,10 @@
+use example_test::hello_to;
+
+#[test]
+fn testit() {
+    main()
+}
+
+fn main() {
+    let _hello_ferris = hello_to("Ferris");
+}
---
+++ tests/data/futures/src/lib.rs
@@ -0,0 +1,20 @@
+
+#[test]
+pub fn a() {
+    futures::executor::ThreadPool::new();
+}
+
+#[test]
+pub fn b() {
+    futures::executor::ThreadPool::new();
+}
+
+#[test]
+pub fn c() {
+    futures::executor::ThreadPool::new();
+}
+
+#[test]
+pub fn d() {
+    futures::executor::ThreadPool::new();
+}
